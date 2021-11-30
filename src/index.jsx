08ef5f5203beceb0eb167dd6ef866266916fcd52
Blake Netzeband <blake.netzeband@kinandcarta.com>
2021-11-30 11:37:26 -0700

    use wasm in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,34 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+const wasm = import("../build/rusty_react");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+wasm.then((m) => {
+  const App = () => {
+    const [name, setName] = React.useState("");
+    const handleChange = (e) => {
+      setName(e.target.value);
+    };
+    const handleClick = () => {
+      m.welcome(name);
+    };
+
+    return (
+      <>
+        <div>
+          <h1>Hi there</h1>
+          <button onClick={m.big_computation}>Run Computation</button>
+        </div>
+        <div>
+          <input type="text" onChange={handleChange} />
+          <button onClick={handleClick}>Say hello!</button>
+        </div>
+      </>
+    );
+  };
+
+  ReactDOM.render(<App />, document.getElementById("root"));
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
