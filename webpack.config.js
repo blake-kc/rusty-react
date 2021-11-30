690df46dd3fec4cfdaf4509f71dbb6404a4d41b2
Blake Netzeband <blake.netzeband@kinandcarta.com>
2021-11-30 11:31:53 -0700

    create webpack scripts

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.[chunkhash].js",
+  },
+  devServer: {
+    compress: true,
+    port: 8080,
+    hot: true,
+    static: "./dist",
+    historyApiFallback: true,
+    open: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+      filename: "index.html",
+      favicon: "./public/favicon.ico",
+    }),
+  ],
+  mode: "development",
+  devtool: "inline-source-map",
+};
